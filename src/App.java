4b6dbd5a6041fb165b2ffe482c55dfda5f9bf7e6
aaronddaron <113373115+aaronddaron@users.noreply.github.com>
2023-10-28 11:19:51 -0500

    Initial

--- src/App.java
+++ src/App.java
@@ -7,26 +7,23 @@ public class App {
          Session session = null;
         try {
 
-            /*Scanner scan = new Scanner(System.in);
+            Scanner scan = new Scanner(System.in);
             System.out.print("Cell username: ");
             String user = scan.nextLine();
             System.out.print("Cell password: ");
-            String pass = scan.nextLine();*/
+            String pass = scan.nextLine();
 
-            session = Connect.ssh("agg0162", "Bteaclmju2@");
+            session = Connect.ssh(user, pass);
 
-            /*System.out.print("mySQL username: ");
+            System.out.print("mySQL username: ");
             user = scan.nextLine();
             System.out.print("mySQL password: ");
-            pass = scan.nextLine();*/
+            pass = scan.nextLine();
 
-            Connection con = Connect.mySQL(session, "agg0162", "11519913");
-            Customers customers = new Customers(con);
-            Parts parts = new Parts(con);
-            Vehicles vehicles = new Vehicles(con);
-            vehicles.massAdd(1);
+            Connection con = Connect.mySQL(session, user, pass);
+            
             con.close();
-            //scan.close();
+            scan.close();
         } finally {
             // TODO Auto-generated catch block
             if (session != null){
